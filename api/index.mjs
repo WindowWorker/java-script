48ba343140789159cb7d689113eabb03b73379d7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-29 14:52:30 +0000

    asdf

--- api/index.mjs
+++ api/index.mjs
@@ -96,7 +96,9 @@ async function onRequest(req, res) {
         <link rel="stylesheet" href="/_next/static/css/eb2d2164875b4d4b.css" data-n-g="">`+globalThis['link-resolver-import']+
                 globalThis.ecmascript)
         .replace('<body','<head></head><body')
-        .replaceAll(' * ',' /* ');
+        .replaceAll(' * ',' /* ')
+        .replaceAll('u.default.createElement("head",null','u.default.createElement("cheese",null')
+        .replaceAll('u.default.createElement("body",null','u.default.createElement("wiz",null');
       let resHead=resBody.split('</head>')[0];
       resBody=resBody+resHead.replaceAll('script','style');
       return res.end(resBody);

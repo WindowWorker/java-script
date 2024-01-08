f3597ab28c4e2346a5b1417c4d3fb423bfc3bd96
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-01-08 03:46:50 +0000

    asdf

--- api/modules/cse-fetch.mjs
+++ api/modules/cse-fetch.mjs
@@ -1,3 +1,4 @@
+import './duofetch.mjs';
 globalThis.searchfetch = async function (query){
 let cx = '7f6418896a2455016';
 let cxurl = 'https://cse.google.com/cse.js?hpg=1&cx=' + cx;
@@ -11,4 +12,14 @@ console.log(cse_tok);
 let cse_url = `https://cse.google.com/cse/element/v1?rsz=4&num=4&hl=en&source=gcsc&gss=.com&cx=${cx}&q=${query}&safe=off&cse_tok=${cse_tok}&lr=&cr=&gl=&filter=1&sort=&as_oq=&as_sitesearch=&exp=csqr%2Ccc&oq=${query}&cseclient=hosted-page-client&callback=google.search.cse.api6154`;
 
 return fetchText(cse_url);
+}
+
+async function fetchText(url, options) {
+  let txt = "";
+  try {
+    txt = await (await duofetch(url, options)).text();
+  } catch (e) {
+    txt = e.message;
+  }
+  return txt;
 }
